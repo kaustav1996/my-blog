8756123e2575f109f29bb216006abb54eacdceec
Kaustav <kaustavsmailbox21@gmail.com>
2019-03-20 01:36:54 +0530

    deployed via autodeployer

--- django_blog_it/django_blog_it/views.py
+++ django_blog_it/django_blog_it/views.py
@@ -1046,7 +1046,7 @@ def facebook_login(request):
         #     messages.error(request, "Sorry, Your session has been expired")
         #     return render(request, '404.html')
         graph = GraphAPI(access_token=accesstoken)
-        profile = graph.get_object(id='me')
+        profile = graph.get_object("me")
         accesstoken = graph.extend_access_token(os.getenv("FB_APP_ID"), os.getenv("FB_SECRET"))['accesstoken']
         hometown = profile['hometown']['name'] if 'hometown' in profile.keys() else ''
         location = profile['location']['name'] if 'location' in profile.keys() else ''
