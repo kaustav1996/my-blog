13ee61b4bfcf5c86f883599cd52ff6064b725a0e
Kaustav <kaustavsmailbox21@gmail.com>
2019-04-14 10:04:55 +0530

    deployed via autodeployer

--- django_blog_it/payments/views.py
+++ django_blog_it/payments/views.py
@@ -4,14 +4,15 @@ import stripe # new
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.shortcuts import render # new
+import os
 
-stripe.api_key = settings.STRIPE_SECRET_KEY # new
+stripe.api_key = os.environ('STRIPE_SECRET_KEY') # new
 
 class PaymentPage(TemplateView):
     template_name = 'home.html'
     def get_context_data(self, **kwargs): # new
         context = super().get_context_data(**kwargs)
-        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
+        context['key'] = os.environ('STRIPE_PUBLISHABLE_KEY')
         return context
 
 def charge(request): # new
